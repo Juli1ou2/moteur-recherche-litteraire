05482f8fd51ab325c986f7f4742f58b546708a96
Skuskusku13 <danlevy149@gmail.com>
2026-02-12 14:24:10 +0100

    feat(new page) page of books details

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import {Routes} from '@angular/router';
 import {Accueil} from './pages/accueil/accueil';
+import {BookDetail} from './pages/book-detail/book-detail';
 
 export const routes: Routes = [
-  {path: "", component: Accueil, title: "Shajudan Search"}
+  {path: "", component: Accueil, title: "Shajudan Search"},
+  {path: "book-detail", component: BookDetail, title: "Book detail Gutendex"}
 ];
